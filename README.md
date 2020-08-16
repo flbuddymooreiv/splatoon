cb9eee1f409f43c2c989be3d6f909c29a3fc0f2c
Buddy Moore <buddy.moore@gmail.com>
2020-08-16 16:34:30 -0400

    Docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # splatoon
+
+`chmod +x splatoon.py`
+
+Bind a key combination to run the program. Here is an i3 example:
+
+`bindsym $mod+Shift+Ctrl+s exec "/usr/bin/python3 /home/buddy/splatoon/splatoon.py"`
+
+Run your keybind, click 2 points on the mapview with all 10 spiders bound and set in your hotbar:
+
+![Spider Remotes in Hotbar](hotbar.png)
+
+Now you can do anything!
---
+++ splatoon.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from pynput import mouse
+import time
+import pyautogui
+
+vectorx = None
+vectory = None
+flankx = None
+flanky = None
+
+def on_move(x, y):
+    pass
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if pressed:
+        global anchorx, anchory, vectorx, vectory, flankx, flanky
+        if not vectorx and not vectory:
+            vectorx = x
+            vectory = y
+            return True
+        if not flankx and not flanky:
+            flankx = x
+            flanky = y
+        if vectorx and vectory and flankx and flanky:
+            return False
+
+def on_scroll(x, y, dx, dy):
+    pass
+
+# Collect events until released
+with mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    listener.join()
+
+print(vectorx, vectory, flankx, flanky)
+
+points = []
+
+spiders = 10
+deltax = (flankx - vectorx)*2
+deltay = (flanky - vectory)*2
+
+pyautogui.PAUSE = .02
+
+for s in range(0,spiders):
+    dx = (deltax/spiders) * s
+    dy = (deltay/spiders) * s
+    px = flankx + dx
+    py = flanky + dy
+
+    pyautogui.press(str(s+1) if s+1<10 else '0')
+    pyautogui.moveTo(px, py)
+    pyautogui.mouseDown()
+    pyautogui.mouseUp()
+
+pyautogui.press('q')
